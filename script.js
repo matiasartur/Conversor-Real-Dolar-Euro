d22df907b9fa0e79f393fc5d2ea735f58476b1d3
matiasarturr <arturmatias0505@gmail.com>
2026-04-15 17:46:00 -0300

    conversão euro e dolar js

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const valueToConvert = document.querySelector(".currency-value-to-convert")
     const valueConverted = document.querySelector(".currency-value")
 
+    console.log(currencySelect.value)
     const dolarToday = 4.99
+    const euroToday = 5.89
 
-    const convertedValue = inputCurrencyValue / dolarToday
+
+
+    if(currencySelect.value == "dolar") { 
+        valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if(currencySelect.value == "euro") {
+        valueConverted.innerHTML = new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "EUR"
+    }).format(inputCurrencyValue / euroToday)
+    }
 
     valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
+            style: "currency",
+            currency: "BRL"
     }).format(inputCurrencyValue)
 
-    valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
 }
 
 convertButton.addEventListener("click", convertValues)
