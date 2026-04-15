3d72bb86101a84c69d89d65bb81f9b23769c191c
matiasarturr <arturmatias0505@gmail.com>
2026-04-15 17:19:21 -0300

    conversão e formatação de currency js

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const valueToConvert = document.querySelector(".currency-value-to-convert")
+    const valueConverted = document.querySelector(".currency-value")
 
     const dolarToday = 4.99
 
     const convertedValue = inputCurrencyValue / dolarToday
 
-    console.log(convertedValue)
+    valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
 }
 
 convertButton.addEventListener("click", convertValues)
